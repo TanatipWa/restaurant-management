418faf3bcc4c1ebb88aff95513c94ae20b2dbcd2
TanatipWaleeroeksab <asdpoommail@gmail.com>
2022-05-12 22:38:23 +0700

    setup layout and create routes

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/TanatipWa/restaurant-management/database"
+	"github.com/TanatipWa/restaurant-management/routes"
+	"github.com/TanatipWa/restaurant-management/middleware"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
